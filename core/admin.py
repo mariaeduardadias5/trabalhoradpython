e13c15f1d3939754e7ca4aad7cdaca8d217a77a4
Maria Eduarda Dias Alves da Silva <mariaeduardadias2504@gmail.com>
2026-04-09 13:54:43 -0300

    atualizações

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Sala, Item, Reserva
 
-# Register your models here.
+admin.site.register(Sala)
+admin.site.register(Item)
+admin.site.register(Reserva)
